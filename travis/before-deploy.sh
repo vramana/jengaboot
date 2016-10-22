f6f57112fb2b875b7f7ddee1088218cee962cace
Ramana Venkata <idlike2dream@gmail.com>
2016-10-22 06:32:25 +0530

    Remove check

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,5 +1,5 @@
-if [[ "$TRAVIS_TAG" = "" ]]; then exit 0; fi
+# if [[ "$TRAVIS_TAG" = "" ]]; then exit 0; fi
 
 PLATFORM=$([ "$TRAVIS_OS_NAME" == "linux" ] && echo "linux64" || echo "$TRAVIS_OS_NAME")
 
-zip "_build/rebel-$PLATFORM-$TRAVIS_TAG.zip" _build/src/rebel
+zip "_build/rebel-$PLATFORM.zip" _build/src/rebel
