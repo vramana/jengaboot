a38e774140b4593c721c4331233f61c6409cdfac
Ramana Venkata <idlike2dream@gmail.com>
2016-10-22 06:54:39 +0530

    back to test

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -2,4 +2,6 @@
 
 PLATFORM=$([ "$TRAVIS_OS_NAME" == "linux" ] && echo "linux64" || echo "$TRAVIS_OS_NAME")
 
-zip "_build/rebel-$PLATFORM.zip" _build/src/rebel
+echo "$TRAVIS_OS_NAME"
+
+zip "_build/rebel-$PLATFORM.zip" package.json
