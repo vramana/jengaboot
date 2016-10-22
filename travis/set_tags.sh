1b538c30f7b8f055b2e739765dc32fbbf879ccc7
Ramana Venkata <idlike2dream@gmail.com>
2016-10-22 07:10:39 +0530

    test master

--- travis/set_tags.sh
+++ travis/set_tags.sh
@@ -6,7 +6,7 @@ if [ -z "$TRAVIS_TAG" ]; then
 
   # Add tag and push to master.
   git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
-  git push "https://${GH_TOKEN}@${GH_REF}" --tags
+  git push origin --tags
   git fetch origin
 
   echo -e "Done magic with tags.\n"
