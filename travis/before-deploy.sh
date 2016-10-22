2dd9076a834699d0145246881f43b9d53ab2a293
Ramana Venkata <idlike2dream@gmail.com>
2016-10-22 07:50:48 +0530

    Test

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,4 +1,4 @@
-# if [[ "$TRAVIS_TAG" = "" ]]; then exit 0; fi
+if [[ "$TRAVIS_TAG" = "" ]]; then exit 0; fi
 
 PLATFORM=$([ "$TRAVIS_OS_NAME" == "linux" ] && echo "linux64" || echo "$TRAVIS_OS_NAME")
 
